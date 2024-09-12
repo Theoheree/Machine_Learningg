97093fd6c567f7c8dd0b011c8f880932e143cb45
Theoheree <220712066@students.uajy.ac.id>
2024-09-12 14:59:48 +0700

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,13 +1,8 @@
 import streamlit as st
-import pandas as pd
 import pickle
 import os
 
-#load model
-model_directory = r'D:\PEMBELAJARAN MESIN\Modul 1' ##diisi dengan path folder dimana file model berada
-
-# Gunakan os.path.join() untuk menggabungkan direktori dan file model pickle
-model_path = os.path.join(model_directory, 'rf_diabetes_model.pkl')
+model_path = 'rf_diabetes_model.pkl'
 
 if os.path.exists(model_path):
     try:
